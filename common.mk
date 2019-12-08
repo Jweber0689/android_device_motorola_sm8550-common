251034fb5e4a4d7a6d81dbb44fab7f0b740c03c6
Ethan Chen <intervigil@gmail.com>
2019-12-08 15:24:31 -0800

    sm8550-common: Remove leftover PRODUCT_VENDOR_MOVE_ENABLED directive
    Change-Id: I07e660f6fab0734c8e590e86862406c043cfe9dd

--- common.mk
+++ common.mk
@@ -409,8 +409,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
-PRODUCT_VENDOR_MOVE_ENABLED := true
-
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
     firmware_wlanmdsp.otaupdate_symlink \
