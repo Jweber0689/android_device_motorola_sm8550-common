40115713419aba3c5633ce4d5b449f8ee4f29122
SGCMarkus <markusornik@gmail.com>
2020-08-24 17:11:05 +0200

    sm8550-common: implement lineage touch hal
    Change-Id: Ia01c834f037ff7f8e50f02c513cc3de4513bce67

--- common.mk
+++ common.mk
@@ -368,6 +368,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.qti
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.rtwo
+
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
