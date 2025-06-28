0baf56463c62864e8fc33fb0cf3aca0c8535d6f1
Nolen Johnson <johnsonnolen@gmail.com>
2025-06-28 13:37:09 -0400

    sm8550-common: Use Moto AIDL Lineage Touch HAL for HTP
    * Keep our HIDL Touch HAL around purely for Touchscreen gestures.
    * Will rework this at some point into an AIDL.
    
    Change-Id: I01d573f6ac854aa15935b1030808c0cf11bdcbd4

--- common.mk
+++ common.mk
@@ -370,7 +370,10 @@ PRODUCT_PACKAGES += \
 
 # Touch
 PRODUCT_PACKAGES += \
+    vendor.lineage.touch-service.motorola \
     vendor.lineage.touch@1.0-service.moto_sm8550
+
+$(call soong_config_set, MOTOROLA_TOUCH, HIGH_TOUCH_POLLING_PATH, /sys/class/touchscreen/primary/interpolation)
 
 # Update engine
 PRODUCT_PACKAGES += \
